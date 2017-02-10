f58f51f96e0324a0fbe7b1e7933829390f428692
Simon Maurer <moirelein@gmail.com>
2017-02-10 17:37:37 +0000

    rename, remove, and adapt testcases

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 PROGNAME=$(basename $0)
@@ -21,6 +21,8 @@ Test a bunch of ifa scripts
 
 Options:
 -h, --help          display this usage message and exit
+-t GROUP            only test specified group of test cases: 'serial',
+                        'feedback', 'loop', 'parallel', 'mult'
 -p                  print final graph
 -v                  verbose
 
@@ -31,10 +33,15 @@ EOF
 
 v=""
 p=""
+t=""
 while [ $# -gt 0 ] ; do
     case "$1" in
         -h|--help)
             usage
+            ;;
+        -t)
+            t="$2"
+            shift
             ;;
         -p)
             p="-p "
@@ -49,36 +56,50 @@ while [ $# -gt 0 ] ; do
     shift
 done
 
-./single_test.sh $v$p-f json ./test/serial1.json
-./single_test.sh $v$p-f json ./test/serial2.json
-./single_test.sh $v$p-f json -ds ./test/serial3.json
-./single_test.sh $v$p-f gml -ds ./test/serial4_1.gml ./test/serial4_2.gml
-./single_test.sh $v$p-f gml -ds ./test/serial4b_1.gml ./test/serial4_2.gml
-./single_test.sh $v$p-f gml -ds ./test/serial4c_1.gml ./test/serial4_2.gml
-./single_test.sh $v$p-f gml -ds ./test/serial4c_1.gml ./test/serial4_2.gml ./test/serial4c_3.gml
-./single_test.sh $v$p-f json ./test/serial5.json
-./single_test.sh $v$p-f json ./test/feedback1.json
-./single_test.sh $v$p-f json -ds ./test/feedback2.json
-./single_test.sh $v$p-f json -ds -db ./test/feedback3.json
-./single_test.sh $v$p-f gml -ds ./test/feedback4_1.gml ./test/feedback4_2.gml
-./single_test.sh $v$p-f gml -ds ./test/feedback4b_1.gml ./test/feedback4_2.gml
-./single_test.sh $v$p-f gml -ds ./test/feedback4c_1.gml ./test/feedback4_2.gml ./test/serial4c_3.gml
-./single_test.sh $v$p-f gml -ds -db ./test/feedback5_1.gml ./test/feedback5_2.gml
-./single_test.sh $v$p-f gml -ds -db ./test/feedback5b_1.gml ./test/feedback5_2.gml
-./single_test.sh $v$p-f gml -ds -db ./test/feedback5c_1.gml ./test/feedback5_2.gml ./test/serial4c_3.gml
-./single_test.sh $v$p-f json ./test/feedback6.json
-./single_test.sh $v$p-f json -ds -db ./test/feedback7.json
-./single_test.sh $v$p-f json ./test/loop1.json
-./single_test.sh $v$p-f json -ds ./test/loop2.json
-./single_test.sh $v$p-f json ./test/parallel1.json
-./single_test.sh $v$p-f json ./test/parallel2.json
-./single_test.sh $v$p-f json -ds ./test/parallel3.json
-./single_test.sh $v$p-f json ./test/parallel4.json
-./single_test.sh $v$p-f json -ds -db ./test/parallel5.json
-./single_test.sh $v$p-f json -ds ./test/parallel6.json
-./single_test.sh $v$p-f json -ds -db ./test/parallel7.json
-./single_test.sh $v$p-f json ./test/mult1.json
-./single_test.sh $v$p-f json -j circle ./test/mult2.json
-./single_test.sh $v$p-f json -j linear -ds -db ./test/mult2.json
-./single_test.sh $v$p-f json -j circle ./test/mult3.json
-./single_test.sh $v$p-f json -j linear -ds -db ./test/mult3.json
+if [ -z "$t" ] || [ "$t" == "serial" ]; then
+    ./single_test.sh $v$p-f json ./test/serial1.json
+    ./single_test.sh $v$p-f json ./test/serial2.json
+    ./single_test.sh $v$p-f json -ds ./test/serial3.json
+    ./single_test.sh $v$p-f gml -ds ./test/serial4_1.gml ./test/serial4_2.gml
+    ./single_test.sh $v$p-f gml -ds ./test/serial4b_1.gml ./test/serial4_2.gml
+    ./single_test.sh $v$p-f gml -ds ./test/serial4c_1.gml ./test/serial4_2.gml
+    ./single_test.sh $v$p-f gml -ds ./test/serial4c_1.gml ./test/serial4_2.gml ./test/serial4c_3.gml
+    ./single_test.sh $v$p-f json ./test/serial5.json
+    ./single_test.sh $v$p-f json -ds ./test/serial6.json
+    ./single_test.sh $v$p-f json -ds ./test/serial7.json
+fi
+if [ -z "$t" ] || [ "$t" == "feedback" ]; then
+    ./single_test.sh $v$p-f json ./test/feedback1.json
+    ./single_test.sh $v$p-f json -ds ./test/feedback2.json
+    ./single_test.sh $v$p-f json -ds -db ./test/feedback3.json
+    ./single_test.sh $v$p-f gml -ds ./test/feedback4_1.gml ./test/feedback4_2.gml
+    ./single_test.sh $v$p-f gml -ds ./test/feedback4b_1.gml ./test/feedback4_2.gml
+    ./single_test.sh $v$p-f gml -ds ./test/feedback4c_1.gml ./test/feedback4_2.gml ./test/serial4c_3.gml
+    ./single_test.sh $v$p-f gml -ds -db ./test/feedback5_1.gml ./test/feedback5_2.gml
+    ./single_test.sh $v$p-f gml -ds -db ./test/feedback5b_1.gml ./test/feedback5_2.gml
+    ./single_test.sh $v$p-f gml -ds -db ./test/feedback5c_1.gml ./test/feedback5_2.gml ./test/serial4c_3.gml
+    ./single_test.sh $v$p-f json ./test/feedback6.json
+    ./single_test.sh $v$p-f json -ds -db ./test/feedback7.json
+    ./single_test.sh $v$p-f json -ds ./test/feedback8.json
+fi
+if [ -z "$t" ] || [ "$t" == "loop" ]; then
+    ./single_test.sh $v$p-f json ./test/loop1.json
+    ./single_test.sh $v$p-f json -ds ./test/loop2.json
+fi
+if [ -z "$t" ] || [ "$t" == "parallel" ]; then
+    ./single_test.sh $v$p-f json ./test/parallel1.json
+    ./single_test.sh $v$p-f json ./test/parallel2.json
+    ./single_test.sh $v$p-f json -ds ./test/parallel3.json
+    ./single_test.sh $v$p-f json ./test/parallel4.json
+    ./single_test.sh $v$p-f json -ds -db ./test/parallel5.json
+    ./single_test.sh $v$p-f json -ds ./test/parallel6.json
+    ./single_test.sh $v$p-f json -ds -db ./test/parallel7.json
+fi
+if [ -z "$t" ] || [ "$t" == "mult" ]; then
+    ./single_test.sh $v$p-f json ./test/mult1.json
+    ./single_test.sh $v$p-f json -j circle ./test/mult2.json
+    ./single_test.sh $v$p-f json -j linear -ds -db ./test/mult2.json
+    ./single_test.sh $v$p-f json -j circle ./test/mult3.json
+    ./single_test.sh $v$p-f json -j linear -ds -db ./test/mult3.json
+    ./single_test.sh $v$p-f json -ds -db ./test/mult4.json
+fi
